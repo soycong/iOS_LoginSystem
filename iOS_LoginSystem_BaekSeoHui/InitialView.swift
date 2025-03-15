d3ddadfc5db76143d7fef15ad5bf7f1ea20c046e
soy <soysauce1223@naver.com>
2025-03-16 03:41:43 +0900

    [feat] initialView 로그인 UI 추가

--- iOS_LoginSystem_BaekSeoHui/InitialView.swift
+++ iOS_LoginSystem_BaekSeoHui/InitialView.swift
@@ -10,16 +10,44 @@ import SnapKit
 
 final class InitialView: UIView {
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "로그인"
+        label.textColor = .sub
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 30, weight: .black)
+        
+        return label
+    }()
+    
+    private let userEmailTextField = UserDataTextField(type: .email, textFieldBackgroundColor: .sub)
+    private let userPasswordTextField = UserDataTextField(type: .password, textFieldBackgroundColor: .sub)
+    
     private var startButton: UIButton = {
         let button = UIButton()
         
         button.setTitle("시작하기", for: .normal)
         button.setTitleColor(.main, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
         button.setCornerRadius()
         button.backgroundColor = .sub
+        button.setHeight()
         
         return button
+    }()
+    
+    private lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel,
+                                                       userEmailTextField,
+                                                       userPasswordTextField,
+                                                       startButton])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        
+        return stackView
     }()
     
     override init(frame: CGRect) {
@@ -34,11 +62,10 @@ final class InitialView: UIView {
     }
     
     private func configureUI() {
-        addSubview(startButton)
+        addSubview(verticalStackView)
         
-        startButton.snp.makeConstraints { make in
+        verticalStackView.snp.makeConstraints { make in
             make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(16)
-            make.height.equalTo(48)
             make.centerX.centerY.equalToSuperview()
         }
     }

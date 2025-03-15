ab97e6cdc54bed8ad9949afb7d19d042b48238b3
soy <soysauce1223@naver.com>
2025-03-16 02:17:01 +0900

    [feat] MainView 인스턴스 생성

--- iOS_LoginSystem_BaekSeoHui/MainViewController.swift
+++ iOS_LoginSystem_BaekSeoHui/MainViewController.swift
@@ -5,3 +5,13 @@
 //  Created by seohuibaek on 3/16/25.
 //
 
+import UIKit
+
+final class MainViewController: UIViewController {
+    private let mainView = MainView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = mainView
+    }
+}

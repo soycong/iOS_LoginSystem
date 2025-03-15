1c71bd6976cfef4cf39126bd386634b9382acd95
soy <soysauce1223@naver.com>
2025-03-15 23:35:03 +0900

    [feat] 시작하기 버튼 선언 및 오토레이아웃 설정

--- iOS_LoginSystem_BaekSeoHui/InitialView.swift
+++ iOS_LoginSystem_BaekSeoHui/InitialView.swift
@@ -5,3 +5,41 @@
 //  Created by seohuibaek on 3/15/25.
 //
 
+import UIKit
+import SnapKit
+
+final class InitialView: UIView {
+    
+    private var startButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("시작하기", for: .normal)
+        button.setTitleColor(.main, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setCornerRadius()
+        button.backgroundColor = .sub
+        
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .main
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        addSubview(startButton)
+        
+        startButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(48)
+            make.centerX.centerY.equalToSuperview()
+        }
+    }
+}

1a777aa1d393485b3381dfbad266ada5f92e05a7
soy <soysauce1223@naver.com>
2025-03-16 04:46:19 +0900

    [feat] 사용자 식별을 위한 이메일 데이터를 저장하는 속성과 초기화 메서드 구현

--- iOS_LoginSystem_BaekSeoHui/MainViewController.swift
+++ iOS_LoginSystem_BaekSeoHui/MainViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 final class MainViewController: UIViewController {
     private let mainView = MainView()
     
+    
+    private var userEmail: String
+    
+    init(email: String) {
+        self.userEmail = email
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view = mainView
     }
+    
+    
 }

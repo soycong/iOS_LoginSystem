6e0645e4cb5a23aa79ca196367eb2267ca642c80
soy <soysauce1223@naver.com>
2025-03-16 19:16:55 +0900

    [refactor] 폴더링 작업 진행

---
+++ iOS_LoginSystem_BaekSeoHui/Screens/Signup/UserDataTextFieldType.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpFieldType.swift
+//  iOS_LoginSystem_BaekSeoHui
+//
+//  Created by seohuibaek on 3/16/25.
+//
+
+enum UserDataTextFieldType {
+    case email
+    case password
+    case confirmPassword
+    case nickname
+}
